33124706ee0d51777945e3249599b02bbc712581
rupak karmakar <pradiprup37@gmail.com>
2026-01-06 21:25:34 +0530

    databaseconnected

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB=async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host} \n`);
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1);
+    }   
+};
+
+export default connectDB;
