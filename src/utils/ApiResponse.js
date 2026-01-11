17deacfc59962f513e5470ae27ebe8351446ec22
rupak karmakar <pradiprup37@gmail.com>
2026-01-11 23:22:45 +0530

    controller done

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,10 @@
 class ApiResponse {
-    constructor(success, message, data = "Success") {
+    constructor(statusCode, data, message = "Success") {
         this.statusCode = statusCode;
-        this.success = statusCode < 400;   
-        this.message = message;
         this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;  
     }
 }
+
+export {ApiResponse}
